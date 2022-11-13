598d20130593762e5510ec81210baf2f0cda26ca
Nafay <nafay2002rizwani@gmail.com>
2022-11-14 04:01:22 +0500

    view Report + AddMedicine View Added

--- src/main/java/com/Pharmacy/Project/Controllers/AdminController.java
+++ src/main/java/com/Pharmacy/Project/Controllers/AdminController.java
@@ -1,5 +1,32 @@
 package com.Pharmacy.Project.Controllers;
 
-public class AdminController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.util.Objects;
+
+public class AdminController {
+    @FXML
+    private Button reportBtn;
+    @FXML
+    private Button addBtn;
+
+    public void showReport(ActionEvent event) throws IOException {
+        Stage stage = (Stage) reportBtn.getScene().getWindow();
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/Pharmacy/Project/viewReport.fxml")));
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+    public void addMedicine(ActionEvent event) throws IOException {
+        Stage stage = (Stage) reportBtn.getScene().getWindow();
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/Pharmacy/Project/AddMedicinePage.fxml")));
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 }
